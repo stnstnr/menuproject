9651c22aad0f38d7dfbed8d6b4437f9a24183a39
Austin Stoner <stnstnr@gmail.com>
2021-03-04 11:24:11 -0700

    initial

---
+++ Assets/Scripts/OpenBrowser.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenBrowser : MonoBehaviour
+{
+    public void ButtonClick(string strain)
+    {
+        Application.OpenURL("https://www.leafly.com/search?q=strawberry%20banana&page=1");
+    }
+}
